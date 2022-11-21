2b0b24756fc98d2081e89609c8efbed170c83ca9
serenakaila <115103250+serenakaila@users.noreply.github.com>
2022-11-21 13:17:15 -0500

    Add files via upload

---
+++ main-12.cs
@@ -0,0 +1,16 @@
+//Program to print even values between 0-10
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int i; //initialization
+    for(i=0; i<=10; i+=2)
+
+    
+    {
+      
+    Console.WriteLine (i);
+    } 
+    //Console.WriteLine ("Hello World");
+  }
+}
