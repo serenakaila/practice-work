af2b8e6cd1238540b6973dbe86d782c32916ab29
serenakaila <115103250+serenakaila@users.noreply.github.com>
2022-11-23 12:49:19 -0500

    Add files via upload

---
+++ main-13.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+ int i; //initialization
+    for(i=10; i>0; i--)
+
+
+    
+    Console.WriteLine (i);
+  }
+}
---
+++ main-19.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int i=0; //initialization
+    while(i<5)
+    {
+  Console.WriteLine(i);
+  i++;
+    }
+  }
+}
